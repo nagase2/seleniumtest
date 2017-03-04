8e5691b2eb699d01883aaffc93a18e74a32801fe
yoshi-PC\yoshi <nagase2@gmail.com>
2017-03-04 22:14:24 +0900

    ３つのWebdriverのテスト成功

--- src/test/java/Selenium3Sample.java
+++ src/test/java/Selenium3Sample.java
@@ -5,42 +5,61 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Selenium3Sample {
 	@Test
-	public void firefoxTest() throws InterruptedException {
+	public void chromeTest() throws InterruptedException {
+		
+		
 		System.setProperty("webdriver.chrome.driver", "./exe/chromedriver.exe");
 		//DesiredCapabilities cap = DesiredCapabilities.firefox();
 	//	cap.setCapability("marionette", true);
 		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://www.google.com");
+		driver.navigate().to("http://corporate.fromjapan.co.jp/ja/");
 		
 		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-		driver.findElement(By.id("lst-ib")).sendKeys("Selenium4");
-		driver.findElement(By.id("lst-ib")).submit();
+//		driver.findElement(By.id("lst-ib")).sendKeys("Selenium4");
+//		driver.findElement(By.id("lst-ib")).submit();
 		System.out.println("★"+driver.getTitle());
 		
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		if(driver!=null) {
-			driver.close();
+			System.out.println("ドライバーを終了します。");
+			driver.quit();
+			//driver.close();
 		}
 	}
 	
-//	@Test
-//	public void firefoxTest2() throws InterruptedException {
-//		System.setProperty("webdriver.gecko.driver", "./exe/geckodriver.exe");
-//		//DesiredCapabilities cap = DesiredCapabilities.firefox();
-//	//	cap.setCapability("marionette", true);
-//		WebDriver driver = new FirefoxDriver();
-//		driver.navigate().to("http://www.google.com");
-//		
-//		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-//		//driver.findElement(By.id("lst-ib")).sendKeys("Selenium4");
-//		//driver.findElement(By.id("lst-ib")).send;
-//		System.out.println("★"+driver.getTitle());
-//		if(driver!=null) {
-//			driver.close();
-//		}
-//	}
+	@Test
+	public void firefoxTest2() throws InterruptedException {
+		
+		//System.setProperty("webdriver.gecko.driver", "./exe/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.navigate().to("http://corporate.fromjapan.co.jp/ja/");
+		
+		System.out.println("★"+driver.getTitle());
+		if(driver!=null) {
+			System.out.println("ドライバーを終了します。");
+			driver.quit();
+		}
+	}
+	@Test
+	
+	public void phantomTest() throws InterruptedException {
+		System.setProperty("phantomjs.binary.path", "./exe/phantomjs.exe");
+		
+		WebDriver driver = new PhantomJSDriver();
+		
+		driver.navigate().to("http://corporate.fromjapan.co.jp/ja/");
+		
+		System.out.println("★"+driver.getTitle());
+		if(driver!=null) {
+			System.out.println("ドライバーを終了します。");
+			driver.quit();
+		}
+	}
+	
 }
